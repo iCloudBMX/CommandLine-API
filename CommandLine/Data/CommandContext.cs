33a4c85bd8957a7f469d6c03dacac4965bace139
iCloudBMX <iCloudBMX0525@gmail.com>
2021-07-19 14:45:15 +0500

    Initial commit

---
+++ CommandLine/Data/CommandContext.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandLine.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        { }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
---
+++ CommandLine/Profiles/CommandsProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CommandLine.Dtos;
+using CommandLine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandLine.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<CommandCreatedDto, Command>();
+
+            CreateMap<Command, CommandReadDto>();
+
+            CreateMap<CommandUpdateDto, Command>();
+        }
+
+    }
+}
